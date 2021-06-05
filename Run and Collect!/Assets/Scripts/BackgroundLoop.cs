4a821c620210f99aa6f92a73dc5d5cc34055fff2
Ayşe Oduncu <oduncu981@gmail.com>
2021-06-06 01:08:44 +0300

    Game (not finished)

---
+++ Run and Collect!/Assets/Scripts/BackgroundLoop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackgroundLoop : MonoBehaviour
+{
+    public static BackgroundLoop instance;
+    private void Awake()
+    {
+        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
+
---
+++ Run and Collect!/Assets/Scripts/ToZombie.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToZombie : MonoBehaviour
+{
+    public void toZombie()
+    {
+        SceneManager.LoadScene("Zombie");
+    }
+}
